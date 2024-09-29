5bd224e6069823a0318440ed35c0f99f3d60d702
cr0w312 <cr0w312@gmail.com>
2024-09-29 16:48:36 +0300

    add validation errors

---
+++ lang/ru/validation.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'required' => 'Поле :attribute обязательно для заполнения.',
+    'min' => [
+        'array' => 'The :attribute field must have at least :min items.',
+        'file' => 'The :attribute field must be at least :min kilobytes.',
+        'numeric' => 'Минимальное количество символов для поля :attribute - :min.',
+        'string' => 'Минимальное количество символов для поля :attribute - :min.',
+    ],
+    'email' => 'Поле :attribute должно содержать валидный адрес электронной почты.',
+    'attributes' => [
+        'email' => 'Емейл',
+        'name' => 'имя',
+    ],
+];
