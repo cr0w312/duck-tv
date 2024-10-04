f7c236eb84e1cf9a1d4a53eaa6862918cd5bd03e
Vladimir <cr0w312@gmail.com>
2024-10-04 17:06:25 +0300

    init filament

--- app/Http/Controllers/NewApplicationController.php
+++ app/Http/Controllers/NewApplicationController.php
@@ -1,33 +1,29 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\NewApplicationRequest;
 use App\Repositories\Interfaces\ApplicationRepositoryInterface;
 
-
 class NewApplicationController extends Controller
 {
-    private $applicationRepo;
+	private $applicationRepo;
 
-    public function __construct(
-        ApplicationRepositoryInterface $applicationRepository
-    )
-    {
-        $this->applicationRepo = $applicationRepository;
-    }
+	public function __construct(
+		ApplicationRepositoryInterface $applicationRepository
+	)
+	{
+		$this->applicationRepo = $applicationRepository;
+	}
 
-    public function store(NewApplicationRequest $request)
-    {
-        $this->applicationRepo->store($request);
-        
-        if($request->wantsJson()){
-            
-            return response()->json(['message'=>'Ваша заявка успешно создана!'], 201);
+	public function store(NewApplicationRequest $request)
+	{
+		$this->applicationRepo->store($request);
+		
+		if($request->wantsJson()){
+			return response()->json(['message'=>'Ваша заявка успешно создана!'], 201);
+		}
 
-        }
-
-        return back()->with(['message'=>'Ваша заявка успешно создана!']);
-    }
+		return back()->with(['message'=>'Ваша заявка успешно создана!']);
+	}
 }
